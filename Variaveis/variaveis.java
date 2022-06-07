716b26799ea7160bb0f617f0da11bd9ac3a39dcc
diegoMarks <diegomarks1806@gmail.com>
2022-06-06 23:15:51 -0300

    Aula de variaveis de tipo primitivo

---
+++ Variaveis/variaveis.java
@@ -0,0 +1,42 @@
+package Variaveis;
+
+public class variaveis {
+    public static void main(String[] args) {
+        //Variaveis do tipo Inteiro
+        //Tipo nome;
+        byte numeroPequeno = 12;
+        short numeroShort = 32767;
+        int numeroMaior = 2147483647;
+        //numero do tipo Long tem quer ter o 'L' no final do numero
+        long numeroEnorme = 922337203685477000l;
+
+        System.out.println(numeroPequeno);
+        System.out.println(numeroShort);
+        System.out.println(numeroMaior);
+        System.out.println(numeroEnorme);
+
+
+        //Variaeis de tipo Ponto Flutuante
+        //Float tem que ter o 'f' no final do numero
+        float peso = 78.7f;
+        double pi = 3.1443543853945943843985434;
+
+
+        //Variaveis do tipo Caractere
+        //Char tem que ser posto em aspas simples ''
+        //So da pra salvar um valo dentro do char
+        char letra = 'c';
+
+
+        //Variavel Booleana
+        //So pode armazenar dois valores, (True ou False) True -> (VERDADEIRO) False -> (FALSO)
+        boolean estouComFome = true;
+
+       //Variavel do tipo String
+       //Tem que estar dentro de aspas duplas
+       String nomeCompleto = "Diego Marks";
+        
+
+       System.out.println(nomeCompleto);
+    }
+}
